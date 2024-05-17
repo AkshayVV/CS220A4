0bdbbe083856437bc12f9b60b83b00c02ac2b591
AkshayVV <119917288+AkshayVV@users.noreply.github.com>
2024-05-17 14:09:46 +1200

    Debugged AS4Q1, deleted A3Q3
    Debugged AS4Q1.py and tested it with samplein.txt. Works well, running time seems to be fine!

--- AS4Q1.py
+++ AS4Q1.py
@@ -1,65 +1,58 @@
 import sys
-def main():
-    limit = int(sys.stdin.readline().strip())
-    overall_min = 0
-    while limit != 0:
-        digraph = []
-        for i in range(limit):
-            number = sys.stdin.readline().split()
-            number = [int(x) for x in number]
-            digraph.append(number)
-        for i in range(len(digraph)):
-            index_node = i
-            back_arc_array = back_arc_find(digraph, index_node)
-            distance_array = distance(digraph)
-            current_min = find_cycle(distance_array, back_arc_array, digraph, index_node)
-        if current_min < overall_min:
-            overall_min = current_min
-        print(overall_min)
-        limit = int(sys.stdin.readline().strip())
-        
+from collections import deque
 
-    
- 
-def distance(digraph):
-    for i in range(len(digraph)):
-        d_list = breadth_first_visit(digraph, i)
-    return d_list
- 
-def back_arc_find(digraph, index_node):
-    array = []
-    for j in range(len(digraph)):
-        for arc in digraph[j]:
-            if index_node == arc:
-                array.append(index_node)
-    return array
- 
-def breadth_first_visit(digraph, s):
-    queue = []
-    colour = ["W"] * len(digraph)
-    pred = [None] * len(digraph)
-    d = [None] * len(digraph)
-    colour[s] = "G"
-    d[s] = 0
-    queue.append(s)
-    while not len(queue) == 0:
-        u = queue[0]
-        a_list = digraph[u]
-        for v in a_list:
-            if colour[v] == "W":
-                colour[v] = "G"
-                pred[v] = u
-                d[v] = d[u] + 1
+def main():
+    while True:
+        limit = int(sys.stdin.readline().strip())
+        if limit == 0:
+            break
+
+        digraph = []
+        for _ in range(limit):
+            line = sys.stdin.readline().strip().split()
+            digraph.append([int(x) for x in line])
+
+        overall_min = float('inf')
+        for i in range(len(digraph)):
+            distances = bfs(digraph, i)
+            back_arcs = find_back_arcs(digraph, i)
+            current_min = find_cycle(distances, back_arcs)
+
+            if current_min < overall_min:
+                overall_min = current_min
+
+        if overall_min == float('inf'):
+            print(0)
+        else:
+            print(overall_min)
+
+def bfs(digraph, start):
+    n = len(digraph)
+    distances = [-1] * n
+    queue = deque([start])
+    distances[start] = 0
+
+    while queue:
+        u = queue.popleft()
+        for v in digraph[u]:
+            if distances[v] == -1:
+                distances[v] = distances[u] + 1
                 queue.append(v)
-        queue.pop(0)
-        colour[u] = "B"  
-    return d
- 
-def find_cycle(distance, back_arc_array, digraph, i):
-    empty_array = []
-    for node in back_arc_array:
-        empty_array.append(distance[node])
-    print(empty_array)
-    return min(empty_array) + 1
- 
+
+    return distances
+
+def find_back_arcs(digraph, index_node):
+    back_arcs = []
+    for j in range(len(digraph)):
+        if index_node in digraph[j]:
+            back_arcs.append(j)
+    return back_arcs
+
+def find_cycle(distances, back_arcs):
+    cycle_lengths = [distances[node] + 1 for node in back_arcs if distances[node] != -1]
+    if cycle_lengths:
+        return min(cycle_lengths)
+    else:
+        return float('inf')
+
 main()
